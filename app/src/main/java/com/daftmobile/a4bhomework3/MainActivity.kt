5ff1c92463a90fd132f66de723efb20a285aceed
Sir Alexsearch <aleksander.repnikov@gmail.com>
2018-11-27 21:23:12 +0100

    remove unused imports

--- app/src/main/java/com/daftmobile/a4bhomework3/MainActivity.kt
+++ app/src/main/java/com/daftmobile/a4bhomework3/MainActivity.kt
@@ -2,7 +2,6 @@ package com.daftmobile.a4bhomework3
 
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
